d2d1d3cf5ef840d77614571200f32194c81d4dee
bronifty <bronifty@gmail.com>
2023-10-20 00:30:25 -0400

    this

--- stream-consumers.mjs
+++ stream-consumers.mjs
@@ -1,11 +1,11 @@
-import { arrayBuffer, blob, json, text } from 'stream-consumers'
-console.log( arrayBuffer, blob, json, text);
+import { arrayBuffer, blob, json, text } from "stream-consumers";
+import { get } from "https";
+console.log(arrayBuffer, blob, json, text);
 
-import path from 'path'
-import { createReadStream } from 'fs'
+import path from "path";
+import { createReadStream } from "fs";
 
-
-const __dirname = new URL('.', import.meta.url).pathname;
-const readable = createReadStream(path.join(__dirname, 'wing.png'))
-const data = await blob(readable)
-console.log(data)
+const __dirname = new URL(".", import.meta.url).pathname;
+const readable = createReadStream(path.join(__dirname, "wing.png"));
+const data = await blob(readable);
+console.log(data.value.toString());
